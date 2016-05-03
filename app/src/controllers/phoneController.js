1bdfc123cdd9dd7aeb1b942b4108a701911993b9
snollygolly <snollygolly@gmail.com>
2016-05-02 21:39:27 -0500

    made babel actually do some work

--- app/src/controllers/phoneController.js
+++ app/src/controllers/phoneController.js
@@ -1,12 +1,12 @@
-var phonecatApp = angular.module('phonecatApp', []);
+const phonecatApp = angular.module("phonecatApp", []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
+phonecatApp.controller("PhoneListCtrl", ($scope) => {
 	$scope.phones = [
-		{'name': 'Nexus S',
-		 'snippet': 'Fast just got faster with Nexus S.'},
-		{'name': 'Motorola XOOM™ with Wi-Fi',
-		 'snippet': 'The Next, Next Generation tablet.'},
-		{'name': 'MOTOROLA XOOM™',
-		 'snippet': 'The Next, Next Generation tablet.'}
+		{"name": "Nexus S",
+		 "snippet": "Fast just got faster with Nexus S."},
+		{"name": "Motorola XOOM™ with Wi-Fi",
+		 "snippet": "The Next, Next Generation tablet."},
+		{"name": "MOTOROLA XOOM™",
+		 "snippet": "The Next, Next Generation tablet."}
 	];
 });
